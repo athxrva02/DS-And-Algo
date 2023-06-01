f3e48aefe2163f914b3bfba2584f0a72b1bd6c97
Atharva Dagaonkar <dagaonkaratharva@gmail.com>
2023-06-02 00:23:55 +0530

    Add insert, get, set_value and pop_front

--- 02_LinkedLists/ll_constructor.py
+++ 02_LinkedLists/ll_constructor.py
@@ -53,19 +53,61 @@ class LinkedList:
         return curr.value
     
     def pop_front(self):
-        pass
+        if self.length == 0:
+            return None
+        curr = self.head
+        self.head = self.head.next
+        curr.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return curr.value
+    
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        curr = self.head
+        idx = 0
+        while idx != index:
+            curr = curr.next
+            idx += 1
+        return curr
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
 
+    def remove(self, index):
+        pass
 
     def print_list(self):
         curr = self.head
         while curr:
             print(curr.value)
             curr = curr.next
-    
 
 lls = LinkedList(10)
-# lls.append(20)
-# lls.append(30)
-lls.pop()
-lls.prepend(100)
+lls.print_list()
+lls.append(5)
+print(lls.get(0))
+print(lls.set_value(1,55))
+lls.insert(1, 22)
 lls.print_list()
