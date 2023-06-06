d78d55429304b24481f8c77e093dd76e21413f34
Atharva Dagaonkar <dagaonkaratharva@gmail.com>
2023-06-06 09:46:37 +0530

    Complete findKthNode in LL

---
+++ 02_LinkedLists/findKthNodeFromEnd.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+
+        
+    def append(self, value):
+        new_node = Node(value)
+        if self.head == None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        return True
+  
+  
+def find_kth_from_end(self, k):
+    slow = self.head
+    fast = self.head
+    for _ in range(k):
+        if not fast:
+            return None
+        fast = fast.next
+    while fast:
+        fast = fast.next
+        slow = slow.next
+    return slow
+        
+        
+
+
+
+
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.append(4)
+my_linked_list.append(5)
+
+
+k = 6
+result = find_kth_from_end(my_linked_list, k)
+
+print(result.value)  # Output: 4
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    4
+    
+"""
+
