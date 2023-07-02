ad9919dd718190be5ff8519b107fb2e04032e087
Atharva Dagaonkar <dagaonkaratharva@gmail.com>
2023-07-02 18:40:11 +0530

    Add Stack

---
+++ XX_Java_Kunal/Stack1.java
@@ -0,0 +1,16 @@
+package XX_Java_Kunal;
+
+import java.util.*;
+
+public class Stack1 {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        System.out.println(st.empty());
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        System.out.println(st.peek());
+        System.out.println(st.search(3));
+    }
+}
---
+++ XX_Java_Kunal/hashMap.java
@@ -0,0 +1,23 @@
+package XX_Java_Kunal;
+import java.util.*;
+
+public class hashMap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Alisson Becker", 1);
+        map.put("Trent Alexander-Arnold", 66);
+        map.put("Virgil van Dijk", 4);
+        map.put("Joe Gomez", 12);
+        map.put("Andrew Robertson", 26);
+        map.put("Jordan Henderson", 14);
+        map.put("Georginio Wijnaldum", 5);
+        map.put("Thiago Alcantara", 6);
+
+        // System.out.println(map);
+        // for(Map.Entry m : map.entrySet())
+        // {
+        //     System.out.println(m.getKey() + " " + m.getValue());
+        // }
+        System.out.println(map.get("Alisson Becker"));
+    }
+}
